b0a8220fba2a7c9d84926fabb84a2dce9ee821f7
AlexLeXX <alexlexx444@yandex.ru>
2022-12-19 20:36:13 +0500

    add realisation im App class

--- fabrickTask/src/main/java/org/example/App.java
+++ fabrickTask/src/main/java/org/example/App.java
@@ -1,9 +1,29 @@
 package org.example;
 
+import org.example.cppDeveloper.CppDeveloperFactory;
+import org.example.javaDeveloper.JavaDeveloperFactory;
+import org.example.phpDeveloper.PhpDeveloperFactory;
+
 public class App
 {
     public static void main( String[] args )
     {
+        DeveloperFactory developerFactory = createDeveloperBySpec("java");
+        Developer developer = developerFactory.createDeveloper();
 
+        developer.WriteCode();
+    }
+
+    static DeveloperFactory createDeveloperBySpec (String spec) {
+        switch (spec) {
+            case "java":
+                return new JavaDeveloperFactory();
+            case "c++":
+                return new CppDeveloperFactory();
+            case "php":
+                return new PhpDeveloperFactory();
+            default:
+                throw new RuntimeException("specialty [" + spec + "] not exist.");
+        }
     }
 }
