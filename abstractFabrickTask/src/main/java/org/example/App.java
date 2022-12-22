4ed73eb55424a03fc02cc952b53e4f302046e5b5
AlexLeXX <alexlexx444@yandex.ru>
2022-12-22 11:33:14 +0500

    final resolve in main class

--- abstractFabrickTask/src/main/java/org/example/App.java
+++ abstractFabrickTask/src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.example.banking.BankingTeamFactory;
+import org.example.website.WebsiteTeamFactory;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ProjectTeamFactory projectBankFactory = new BankingTeamFactory();
+        Developer bankDeveloper = projectBankFactory.getDeveloper();
+        Tester bankTester = projectBankFactory.getTester();
+        ProjectManager bankPM = projectBankFactory.getProjectManager();
+
+        System.out.println("Banking team:");
+        bankDeveloper.writeCode();
+        bankTester.testCode();
+        bankPM.manageProject();
+
+        System.out.println("\n");
+
+        ProjectTeamFactory projectWebsiteFactory = new WebsiteTeamFactory();
+        Developer websiteDeveloper = projectWebsiteFactory.getDeveloper();
+        Tester websiteTester = projectWebsiteFactory.getTester();
+        ProjectManager websitePM = projectWebsiteFactory.getProjectManager();
+
+        System.out.println("Website team:");
+        websiteDeveloper.writeCode();
+        websiteTester.testCode();
+        websitePM.manageProject();
     }
 }
